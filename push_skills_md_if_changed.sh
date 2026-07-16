e72d3c121730cd081e891066a588c9f4d916ab7a
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-16 16:56:21 -0400

    Fixed push_skills_md_if_changed.sh

--- push_skills_md_if_changed.sh
+++ push_skills_md_if_changed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(git diff skills.md src/main/resources/header.md src/main/java/org/example/Main.java)" ]; then
+if [ "$(git diff skills.md)" ]; then
   git add skills.md
   git commit -m "skills.md added by GitHub Actions"
   git push origin master
