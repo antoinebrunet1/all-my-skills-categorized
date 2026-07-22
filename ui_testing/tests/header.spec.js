a80bbfad5c46555ce6e371c2aa83fbe50f46fb93
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-22 19:24:41 -0400

    Added selectors in test_data.json

--- ui_testing/tests/header.spec.js
+++ ui_testing/tests/header.spec.js
@@ -7,10 +7,10 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Header has correct text', async function ({ page }) {
-    const h1Locator = await page.locator('h1')
-    const h2Locator = await page.locator('h2')
-    const pLocator = await page.locator('p')
-    const liLocator = await page.locator('//ul/li')
+    const h1Locator = await page.locator(testData.selectors.h1)
+    const h2Locator = await page.locator(testData.selectors.h2)
+    const pLocator = await page.locator(testData.selectors.p)
+    const liLocator = await page.locator(testData.selectors.li)
 
     await checkIfLocatorHasText(h1Locator, testData.expectedText.h1)
     await checkIfLocatorHasText(h2Locator, testData.expectedText.h2)
@@ -21,10 +21,10 @@ test('Header has correct text', async function ({ page }) {
 });
 
 test('Header sections are in correct order', async function ({ page }) {
-    const h1Locator = await page.locator('h1')
-    const h2Locator = await page.locator('h2')
-    const pLocator = await page.locator('p')
-    const ulLocator = await page.locator('ul').first()
+    const h1Locator = await page.locator(testData.selectors.h1)
+    const h2Locator = await page.locator(testData.selectors.h2)
+    const pLocator = await page.locator(testData.selectors.p)
+    const ulLocator = await page.locator(testData.selectors.ul).first()
 
     await checkIfIsAbove(h1Locator, h2Locator)
     await checkIfIsAbove(h2Locator, pLocator)
