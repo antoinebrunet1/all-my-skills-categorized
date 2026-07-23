716da8f2f1d2f344e2dd3205a5952edcbe96b605
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-23 19:33:04 -0400

    Fixed push_skills_md_if_changed_and_ui_tests_pass.sh

--- push_skills_md_if_changed_and_ui_tests_pass.sh
+++ push_skills_md_if_changed_and_ui_tests_pass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(git diff skills.md)" && "$(make run-ui-tests-ci)" ]; then
+if [ "$(git diff skills.md)" ] && [ "$(make run-ui-tests-ci)" ]; then
   git add skills.md
   git commit -m "skills.md added by GitHub Actions"
   git push origin master
