71ca33e7d1aaec59cdfb4e5235cee7aa399c6aad
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-22 21:28:44 -0400

    Added List items are in alphabetical order for every list test in after_header.spec.js

--- ui_testing/tests/after_header.spec.js
+++ ui_testing/tests/after_header.spec.js
@@ -12,3 +12,15 @@ test('Titles are in alphabetical order', async function ({ page }) {
     titlesLocators.shift()
     await checkIfElementsAreInAlphabeticalOrder(titlesLocators)
 })
+
+test('List items are in alphabetical order for every list', async function ({ page }) {
+    const listsLocators = await page.locator(testData.selectors.ul).all()
+
+    listsLocators.shift()
+
+    for (const listLocator of listsLocators) {
+        const liLocators = listLocator.locator(testData.selectors.li)
+
+        await checkIfElementsAreInAlphabeticalOrder(liLocators)
+    }
+})
