964e147190148079974989b5cdeecce074d8126d
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-23 20:09:21 -0400

    Moved Setup git config commands to push_skills_md_if_changed_and_ui_tests_pass.sh

--- push_skills_md_if_changed_and_ui_tests_pass.sh
+++ push_skills_md_if_changed_and_ui_tests_pass.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ "$(git diff skills.md)" ] && [ "$(make run-ui-tests-ci)" ]; then
+  git config user.name "GitHub Actions Bot"
+  git config user.email "<>"
   git add skills.md
   git commit -m "skills.md added by GitHub Actions"
   git push origin master
