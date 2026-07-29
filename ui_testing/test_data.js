26585e290486281b518f5551f8b41085c3bd52d6
Antoine Brunet <antoinebrunet1@outlook.com>
2026-07-29 18:07:12 -0400

    Changed bio

--- ui_testing/test_data.js
+++ ui_testing/test_data.js
@@ -3,7 +3,7 @@ export const testData = {
     expectedTexts: {
         h1: "All my skills categorized",
         h2: "Antoine Brunet",
-        p: "Software developer + Quality assurance analyst // I produce scalable code, create algorithms that solve highly complex problems and guarantee dev/QA quality using pipelines",
+        p: "Software developer & Quality assurance analyst // Producing scalable code. // Creating algorithms for highly complex problems. // Guaranteeing dev/QA quality with pipelines.",
         ul: [
             "LinkedIn profile: https://www.linkedin.com/in/antoinebrunet97/",
             "Email: antoinebrunet1@outlook.com",
